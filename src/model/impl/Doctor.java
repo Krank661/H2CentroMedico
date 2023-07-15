af7eb6b2ba1e1ccd0f39ef91a075d18a2a070448
Krank661 <doushiuou@gmail.com>
2023-07-15 19:30:05 +0200

    código de la clase Médico

--- src/model/impl/Doctor.java
+++ src/model/impl/Doctor.java
@@ -1,6 +1,48 @@
 package model.impl;
 
+import enums.Specialties;
 import model.MedicalProfessional;
 
+import java.util.Objects;
+
 public class Doctor extends MedicalProfessional {
+    private Specialties specialty;
+
+    public Doctor() {
+        super();
+    }
+
+    public Doctor(String dni, String name, String phoneNumber, Integer id, Specialties specialty) {
+        super(dni, name, phoneNumber, id);
+        this.specialty = specialty;
+    }
+
+    public Specialties getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(Specialties specialty) {
+        this.specialty = specialty;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Doctor)) return false;
+        if (!super.equals(o)) return false;
+        Doctor doctor = (Doctor) o;
+        return getSpecialty() == doctor.getSpecialty();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getSpecialty());
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "specialty=" + specialty +
+                "} " + super.toString();
+    }
 }
