e66d02dbb44e74e61a17af6420e10153a7cea243
Krank661 <doushiuou@gmail.com>
2023-07-15 19:32:48 +0200

    código de la clase Enfermera

--- src/model/impl/Nurse.java
+++ src/model/impl/Nurse.java
@@ -3,4 +3,16 @@ package model.impl;
 import model.MedicalProfessional;
 
 public class Nurse extends MedicalProfessional {
+    public Nurse() {
+        super();
+    }
+
+    public Nurse(String dni, String name, String phoneNumber, Integer id) {
+        super(dni, name, phoneNumber, id);
+    }
+
+    @Override
+    public String toString() {
+        return "Nurse{} " + super.toString();
+    }
 }
