5f030499cd93866470fcde53fe38146eb0d84a92
Krank661 <doushiuou@gmail.com>
2023-07-17 10:28:57 +0200

    registro inicial extra

--- create.sql
+++ create.sql
@@ -3,6 +3,8 @@ CREATE TABLE IF NOT EXISTS pacientes (dni VARCHAR(255) PRIMARY KEY, nombre VARCH
 --delete initial records to avoid duplicates
 DELETE FROM pacientes WHERE dni = '666';
 DELETE FROM pacientes WHERE dni = '333';
+DELETE FROM pacientes WHERE dni = '111';
 --insert initial records
 INSERT INTO pacientes(dni, nombre, telefono, grupo_sanguineo, edad) VALUES ('666', 'Alguien', '123456', 'APOS', 19);
 INSERT INTO pacientes(dni, nombre, telefono, grupo_sanguineo, edad) VALUES ('333', 'Anonymous', '654321', 'BPOS', 32);
+INSERT INTO pacientes(dni, nombre, telefono, grupo_sanguineo, edad) VALUES ('111', 'Ichiban', '000000', 'ONEG', 70);
