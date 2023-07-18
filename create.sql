36128349fdcc6f3efd2fd5e4fffae67dd99fc431
Krank661 <doushiuou@gmail.com>
2023-07-18 06:01:19 +0200

    imports corregidos

--- create.sql
+++ create.sql
@@ -1,6 +1,6 @@
 --create the table
 CREATE TABLE IF NOT EXISTS pacientes (dni VARCHAR(255) PRIMARY KEY, nombre VARCHAR(255), telefono VARCHAR(255), grupo_sanguineo VARCHAR(255), edad INT);
-CREATE TABLE IF NOT EXISTS profesionales_de_la_salud(id INT PRIMARY KEY, dni VARCHAR(255), nombre VARCHAR(255), telefono VARCHAR(255), especialidad VARCHAR(255));
+CREATE TABLE IF NOT EXISTS profesionales_de_la_salud(id INT PRIMARY KEY, dni VARCHAR(255), nombre VARCHAR(255), telefono VARCHAR(255), especialidad VARCHAR(255), profesion VARCHAR(255));
 --delete initial records to avoid duplicates
 DELETE FROM pacientes WHERE dni = '666';
 DELETE FROM pacientes WHERE dni = '333';
@@ -12,6 +12,6 @@ DELETE FROM profesionales_de_la_salud WHERE id = 3;
 INSERT INTO pacientes(dni, nombre, telefono, grupo_sanguineo, edad) VALUES ('666', 'Alguien', '123456', 'APOS', 19);
 INSERT INTO pacientes(dni, nombre, telefono, grupo_sanguineo, edad) VALUES ('333', 'Anonymous', '654321', 'BPOS', 32);
 INSERT INTO pacientes(dni, nombre, telefono, grupo_sanguineo, edad) VALUES ('111', 'Ichiban', '000000', 'ONEG', 70);
-INSERT INTO profesionales_de_la_salud(id, dni, nombre, telefono, especialidad, profesion VARCHAR(255)) VALUES(1, '10', 'primero', '1234', 'GENERAL_PRACTICIONER', 'médico');
-INSERT INTO profesionales_de_la_salud(id, dni, nombre, telefono, especialidad, profesion VARCHAR(255)) VALUES(2, '20', 'segundo', '2345', 'NONE', 'paramédico');
-INSERT INTO profesionales_de_la_salud(id, dni, nombre, telefono, especialidad, profesion VARCHAR(255)) VALUES(3, '30', 'tercero', '3456', 'NONE', 'enfermero');
+INSERT INTO profesionales_de_la_salud(id, dni, nombre, telefono, especialidad, profesion) VALUES(1, '10', 'primero', '1234', 'GENERAL_PRACTICIONER', 'médico');
+INSERT INTO profesionales_de_la_salud(id, dni, nombre, telefono, especialidad, profesion) VALUES(2, '20', 'segundo', '2345', 'NONE', 'paramédico');
+INSERT INTO profesionales_de_la_salud(id, dni, nombre, telefono, especialidad, profesion) VALUES(3, '30', 'tercero', '3456', 'NONE', 'enfermero');
